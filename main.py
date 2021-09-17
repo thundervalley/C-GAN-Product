7bba5a0f401736e932bd0a1f91cc90acfc3dbb25
Younghoon-Lee <8109h@naver.com>
2021-09-18 03:26:26 +0900

    first commit

---
+++ main.py
@@ -0,0 +1,14 @@
+import os 
+import torch
+import torchvision
+import torch.nn as nn
+from torchvision import transforms
+from torchvision.utils import save_image
+
+
+def main():
+    pass
+
+
+if __name__ == '__main__':
+    main()
---
+++ models.py
@@ -0,0 +1,11 @@
+import torch
+import torch.nn as nn
+
+class Generator(nn.Module):
+    def __init__(self ):
+        pass
+
+class Discriminator(nn.Module):
+    def __init__(self):
+        pass
+
