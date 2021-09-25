51f3620c5f03fec6179998c9f01fe6c39925a886
ansojung <ququ5444@naver.com>
2021-09-26 03:08:11 +0900

    update

--- models.py
+++ models.py
@@ -1,12 +1,11 @@
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
-latent_size = 64
-hidden_size = 256
-image_size = 784
+
 class Generator(nn.Module):
-    def __init__(self):
+    def __init__(self.latent_size,self.hidden_size,self.image_size):
         super().__init()
+        
         self.G = nn.Sequential(
             nn.Linear(latent_size, hidden_size),
             nn.ReLU(),
@@ -16,23 +15,23 @@ class Generator(nn.Module):
             nn.Tanh()
         )
     def forward(self, x):
-        img=self.G(x)
+        img=self.Generator(x)
         return img
 
 
 
 class Discriminator(nn.Module):
-    def __init__(self):
+    def __init__(self.latent_size,self.hidden_size,self.image_size):
         super().__init()
-    D = nn.Sequential(
-        nn.Linear(image_size,hidden_size),
-        nn.LeakyReLU(0.2),
-        nn.Linear(hidden_size, hidden_size),
-        nn.LeakyReLU(0.2),
-        nn.Linear(hidden_size,1),
-        nn.Sigmoid()
-    )
+        D = nn.Sequential(
+            nn.Linear(image_size,hidden_size),
+            nn.LeakyReLU(0.2),
+            nn.Linear(hidden_size, hidden_size),
+            nn.LeakyReLU(0.2),
+            nn.Linear(hidden_size,1),
+            nn.Sigmoid()
+        )
     def forward(self, x):
-        img=self.D(x)
+        img=self.Discriminator(x)
         return img
 
